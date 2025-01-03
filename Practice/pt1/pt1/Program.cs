6f1ceedc8c3c4adc2e246d17d3e18bfc04523a26
Rahul Dusane <152960290+Rahul-dusane@users.noreply.github.com>
2025-01-03 11:58:02 +0530

    Add files via upload

---
+++ Practice/pt1/pt1/Program.cs
@@ -0,0 +1,58 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static int staticCounter = 0;
+
+        private string instanceMessage;
+
+        public Program(string message) => instanceMessage = message;
+
+        static void DisplayStaticCounter()
+        {
+            Console.WriteLine($"static Counter Value: {staticCounter}");
+        }
+
+        public void DisplayInstanceMessage()
+        {
+            Console.WriteLine($"Instance Message: {instanceMessage}");
+        }
+
+        public int AddNumbers(int a,int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string [] args)
+        {
+            int intNumber = 42;
+            float floatNumber = 3.14f;
+            bool isCodingFun = true;
+            string greeting = "Hello, .NET Core!";
+
+            Console.WriteLine("Primitive Data Type:");
+            Console.WriteLine($"Integer: {intNumber}");
+            Console.WriteLine($"Float: {floatNumber}");
+            Console.WriteLine($"Boolean: {isCodingFun}");
+            Console.WriteLine($"String: {greeting}");
+
+            staticCounter++;
+            DisplayStaticCounter();
+
+            Program Program = new Program("Welcome To C#.net Core!");
+            Program.DisplayInstanceMessage();
+            int sum = Program.AddNumbers(5, 7);
+            Console.WriteLine($"Sum Of 5 and 7: {sum}");
+
+            Console.WriteLine("Program Execution Complete!");
+            
+            return ;
+            
+
+
+        }
+    }
+}
---
+++ Practice/pt2/pt2/Program.cs
@@ -0,0 +1,105 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace AdvanceCSharpProgram
+{
+    class Shape
+    {
+        public virtual void DisplayArea()
+        {
+            Console.WriteLine("Area Of The Shape Is Not Defined.");
+        }
+    }
+
+    class Circle : Shape
+    {
+        public double Radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public override void DisplayArea()
+        {
+            double area = Math.PI * Radius;
+            Console.WriteLine($"Area Of The Circle : {area:F2}");
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        public double Length;
+        public double Width;
+
+        public Rectangle(double length, double width)
+        {
+            Length = length;
+            Width = width;
+        }
+
+        public override void DisplayArea()
+        {
+            double area = Length * Width;
+            Console.WriteLine($"Area Of Rectangle : {area:F2}");
+        }
+
+        public double CalculatePerameter()
+        {
+            return 2 * (Length + Width);
+        }
+    }
+
+    class Program
+    {
+        static void Main(String[] s)
+        {
+            try
+            {
+                int intNumber = 100;
+                double doubleNumber = intNumber;
+                Console.WriteLine($"Implicit Casting (int to double) : {doubleNumber}");
+
+                double anotherDouble = 123.43;
+                int castedInt = (int)anotherDouble;
+                Console.WriteLine($"Explicit Casting (double to int) : {castedInt}");
+
+                string strNumber = "456";
+                int parsedNumebr = int.Parse(strNumber);
+                Console.WriteLine($"Parsed Number: {parsedNumebr}");
+
+                string strDouble = "789.65";
+                double convertedDouble = Convert.ToDouble(strDouble);
+                Console.WriteLine($"Converted Double : {convertedDouble}");
+
+                Shape shape = new Shape();
+                Shape circle = new Circle(5.4);
+                Shape rectangle = new Rectangle(10.0, 20.0);
+
+                Console.WriteLine("\nShape Areas:");
+                shape.DisplayArea();
+                circle.DisplayArea();
+                rectangle.DisplayArea();
+
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Error:Division By Zero Is Not Allowed...! ");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: Invalid Input Format : Please Enter Valid Number...!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Unexpected Error : {ex.Message}");
+            }
+
+            finally
+            {
+                Console.WriteLine("\nProgram Execution Completed.");
+            }
+        }
+    }
+}
